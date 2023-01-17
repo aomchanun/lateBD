d89837122a55faf46c081836ddf52b6e44aa429e
aomsnnn <omsinjah@gmail.com>
2023-01-17 16:37:42 +0700

    lab15.2 completed

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,48 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void showMatrix(const bool B [N][N])
+{
+	for(int i=0;i<N;i++)
+	{
+		for(int j=0;j<N;j++)
+		{
+			cout << B[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
+
+void inputMatrix(double A [N][N])
+{
+	for(int i=0;i<N;i++)
+	{
+		cout << "Row " << i+1 << ": ";
+		for(int j=0;j<N;j++)
+		{
+			cin >> A[i][j];
+		}
+	}
+}
+
+void findLocalMax(const double A[N][N], bool B[N][N])
+{
+	for(int i=0;i<N;i++)
+	{
+		for(int j=0;j<N;j++)
+		{
+			B[i][j] = 0;
+		}
+	}
+	for(int i=1;i<N-1;i++)
+	{
+		for(int j=1;j<N-1;j++)
+		{
+			if(A[i][j]>=A[i][j-1] and A[i][j]>=A[i-1][j] and A[i][j]>=A[i][j+1] and A[i][j]>=A[i+1][j])
+			{
+				B[i][j] = 1;
+			}
+		}
+	}
+}
