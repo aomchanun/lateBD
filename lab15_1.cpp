cb143663fda794faa7719c805912bb78e57903b3
aomsnnn <omsinjah@gmail.com>
2023-01-17 15:47:33 +0700

    lab 15.1 completed

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,23 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	for(int i=1;i<N;i++)
+	{
+		T temp = d[i];
+		int j=i-1;
+		while(j>= 0 and d[j]<temp)
+		{
+			d[j+1] = d[j];
+			j -= 1;
+		}
+		d[j+1]=temp;
+		cout << "Pass " << i << ":";
+		for(int k=0;k<N;k++)
+		{
+			cout << d[k] << " ";
+		}
+		cout << "\n";
+	}
 }
 
 int main(){
@@ -17,3 +33,4 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+
